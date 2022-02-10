d15c43bdd57b971d7a48746362e332b12de47c7e
aresprelate <67679203+aresprelate@users.noreply.github.com>
2022-02-10 16:16:52 +0600

    commit

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log("The Websit Has Started");
-alert("We use cookies!");
-alertify.confirm("This is a confirm dialog.",
-  function(){
-    alertify.success('Ok');
-  },
-  function(){
-    alertify.error('Cancel');
-  });
